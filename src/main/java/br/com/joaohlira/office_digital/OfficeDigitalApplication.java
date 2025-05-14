6e433520493281d58df881619fa281bce28775aa
JoaoHLira <lirarosas.jh@gmail.com>
2025-05-14 18:09:14 -0300

    first commit

---
+++ src/main/java/br/com/joaohlira/office_digital/OfficeDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaohlira.office_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaohlira/office_digital/OfficeDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaohlira.office_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
