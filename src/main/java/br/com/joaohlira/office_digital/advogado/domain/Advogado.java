6c3281b9b33ae9a570a391442fb159d112061a5f
JoaoHLira <lirarosas.jh@gmail.com>
2025-07-21 17:01:57 -0300

    feat: adiciona anotacoes do id do advogado para persistencia e cria construtor personalizado

--- src/main/java/br/com/joaohlira/office_digital/advogado/domain/Advogado.java
+++ src/main/java/br/com/joaohlira/office_digital/advogado/domain/Advogado.java
@@ -1,15 +1,18 @@
 package br.com.joaohlira.office_digital.advogado.domain;
 
-import jakarta.persistence.Table;
+import br.com.joaohlira.office_digital.advogado.application.api.AdvogadoNovoRequest;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
+@Entity
 @Table(name = "advogado")
 public class Advogado {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String nome;
     private String numeroOab;
@@ -17,4 +20,11 @@ public class Advogado {
     private String whatsapp;
     private String email;
 
+    public Advogado(AdvogadoNovoRequest request) {
+        this.nome = request.getNome();
+        this.numeroOab = request.getNumeroOab();
+        this.estadoOab = request.getEstadoOab();
+        this.whatsapp = request.getWhatsapp();
+        this.email = request.getEmail();
+    }
 }
