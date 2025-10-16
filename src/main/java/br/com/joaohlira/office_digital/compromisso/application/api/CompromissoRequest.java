45756abb8a8bede2ba75294e6305251db6106cf4
JoaoHLira <lirarosas.jh@gmail.com>
2025-10-16 14:55:03 -0300

    refactor: remove atributo dataHoraCriacao de CompromissoRequest por nao haver necessidade de existir nele

--- src/main/java/br/com/joaohlira/office_digital/compromisso/application/api/CompromissoRequest.java
+++ src/main/java/br/com/joaohlira/office_digital/compromisso/application/api/CompromissoRequest.java
@@ -3,7 +3,6 @@ package br.com.joaohlira.office_digital.compromisso.application.api;
 import br.com.joaohlira.office_digital.compromisso.domain.TipoCompromisso;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.UUID;
 
@@ -12,6 +11,5 @@ public record CompromissoRequest(LocalDate data,
                             TipoCompromisso tipoCompromisso,
                             String descricaoCompromisso,
                             String numeroProcesso,
-                            LocalDateTime dataHoraCriacao,
                             UUID idAdvogadoResponsavel) {
 }
